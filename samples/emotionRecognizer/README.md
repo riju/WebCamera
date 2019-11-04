256dbaefbdb67af3e236570fb9d133a8a8f97073
Aleksandra Zhuravleva <aleksandra.zhuravleva@intel.com>
2019-11-04 16:49:27 +0200

    [Emotion recognition] Use Fisher faces model and face module from opencv_contrib to recognize emotion and draw it over a face

---
+++ samples/emotionRecognizer/README.md
@@ -0,0 +1,9 @@
+# Goal
+
+
+
+## References
+
+1. [Emotions classifier and demo in OpenCV](https://github.com/PiotrDabrowskey/facemoji)
+2. [Emotions classifier and demo in OpenCV](https://github.com/PanKnst/Emotion-Detection)
+3. [Emotion Recognition With Python, OpenCV and a Face Dataset](http://www.paulvangent.com/2016/04/01/emotion-recognition-with-python-opencv-and-a-face-dataset/)
---
+++ samples/emotionRecognizer/index.js
@@ -0,0 +1,235 @@
+let utils = new Utils('errorMessage');
+let stats = null;
+let controls = {};
+let videoConstraint;
+let streaming = false;
+let videoTrack = null;
+
+let video = document.getElementById('videoInput');
+let canvasOutput = document.getElementById('canvasOutput');
+let canvasInput = null;
+let canvasInputCtx = null;
+
+let src = null;
+let gray = null;
+let faceVec = null;
+let faceCascade = null;
+let fisherFaceRecognizer = null;
+
+const faceModelPath = 'haarcascade_frontalface_default.xml';
+const faceModelUrl = '../../data/classifiers/haarcascade_frontalface_default.xml';
+const emojiModelPath = 'emotion_detection_model.xml';
+const emojiModelUrl = '../../data/classifiers/emotion_detection_model.xml';
+
+// emoticons will have the same order as emotions.
+let emoticons = [];
+const emotions = ['neutral', 'anger', 'disgust', 'fear', 'happiness', 'sadness', 'surprise']
+let nImagesLoaded = 0;
+const N_IMAGES = emotions.length;
+
+
+function initOpencvObjects() {
+  src = new cv.Mat(video.height, video.width, cv.CV_8UC4);
+  gray = new cv.Mat();
+
+  faceVec = new cv.RectVector();
+  faceCascade = new cv.CascadeClassifier();
+  // TODO(sasha): Use Web Workers to load files.
+  faceCascade.load(faceModelPath);
+
+  emotions.forEach(emotion => {
+    let emoticonImg = createImgNode(emotion + '-emoticon');
+
+    emoticonImg.onload = function () {
+      ++nImagesLoaded;
+    };
+    emoticonImg.src = '../../data/emoticons/' + emotion + '.png';
+  });
+  fisherFaceRecognizer = new cv.face_FisherFaceRecognizer();
+  fisherFaceRecognizer.read(emojiModelPath);
+}
+
+function createImgNode(id) {
+  let imgNode = document.createElement('img');
+  imgNode.id = id;
+  imgNode.classList.add('hidden');
+  document.getElementsByTagName('body')[0].appendChild(imgNode);
+  return imgNode;
+}
+
+function deleteOpencvObjects() {
+  src.delete(); gray.delete();
+  faceVec.delete(); faceCascade.delete();
+
+  emoticons.forEach(emoticon => {
+    emoticon.src.delete();
+    emoticon.mask.delete();
+  });
+}
+
+function completeStyling() {
+  let cameraBar = document.querySelector('.camera-bar-wrapper');
+  cameraBar.style.width = `${video.width}px`;
+  document.getElementById('takePhotoButton').disabled = false;
+
+  // Extra canvas to get source image from video element
+  // (instead of cv.VideoCapture).
+  canvasInput = document.createElement('canvas');
+  canvasInput.width = video.width;
+  canvasInput.height = video.height;
+  canvasInputCtx = canvasInput.getContext('2d');
+
+  canvasOutput.width = video.width;
+  canvasOutput.height = video.height;
+}
+
+function waitForResources() {
+  if (nImagesLoaded == N_IMAGES) {
+    emotions.forEach(emotion => {
+      let emoticonImg = document.getElementById(emotion + '-emoticon');
+      let rgbaVector = new cv.MatVector();
+      let emoticon = {};
+      emoticon.src = cv.imread(emoticonImg);
+      cv.split(emoticon.src, rgbaVector); // Create mask from alpha channel.
+      emoticon.mask = rgbaVector.get(3);
+      emoticon.name = emotion;
+      emoticons.push(emoticon);
+      rgbaVector.delete();
+    });
+
+    requestAnimationFrame(processVideo);
+    return;
+  }
+  setTimeout(waitForResources, 50);
+}
+
+function processVideo() {
+  try {
+    if (!streaming) {
+      cleanupAndStop();
+      return;
+    }
+    stats.begin();
+    canvasInputCtx.drawImage(video, 0, 0, video.width, video.height);
+    let imageData = canvasInputCtx.getImageData(0, 0, video.width, video.height);
+    src.data.set(imageData.data);
+
+    // Detect faces.
+    cv.cvtColor(src, gray, cv.COLOR_RGBA2GRAY, 0);
+    faceCascade.detectMultiScale(gray, faceVec);
+
+    for (let i = 0; i < faceVec.size(); ++i) {
+      let face = faceVec.get(i);
+      // Recognize emotion.
+
+      // Detect eyes in face ROI.
+      let faceGray = gray.roi(face);
+      cv.resize(faceGray, faceGray, new cv.Size(350, 350));
+      let prediction = fisherFaceRecognizer.predict_label(faceGray);
+      //console.log(prediction);
+      let emoticon = emoticons[prediction];
+      let newEmoticonSize = new cv.Size(face.width, face.height);
+      let resizedEmoticon = new cv.Mat();
+      let resizedMask = new cv.Mat();
+      cv.resize(emoticon.src, resizedEmoticon, newEmoticonSize);
+      cv.resize(emoticon.mask, resizedMask, newEmoticonSize);
+      resizedEmoticon.copyTo(src.rowRange(face.y, face.y + face.height)
+        .colRange(face.x, face.x + face.width), resizedMask);
+
+      faceGray.delete();
+      resizedEmoticon.delete();
+      resizedMask.delete();
+    }
+    cv.imshow(canvasOutput, src);
+
+    stats.end();
+    requestAnimationFrame(processVideo);
+  } catch (err) {
+    utils.printError(err);
+  }
+};
+
+function startCamera() {
+  if (!streaming) {
+    utils.clearError();
+    utils.startCamera(videoConstraint, 'videoInput', onVideoStartedCustom);
+  } else {
+    utils.stopCamera();
+    onVideoStopped();
+  }
+}
+
+function onVideoStartedCustom() {
+  streaming = true;
+  setMainCanvasProperties(video);
+  videoTrack = video.srcObject.getVideoTracks()[0];
+  imageCapturer = new ImageCapture(videoTrack);
+  document.getElementById('mainContent').classList.remove('hidden');
+  completeStyling();
+  initOpencvObjects();
+  requestAnimationFrame(waitForResources);
+}
+
+function cleanupAndStop() {
+  deleteOpencvObjects();
+  utils.stopCamera(); onVideoStopped();
+}
+
+function initUI() {
+  let menuHeight = parseInt(getComputedStyle(
+    document.querySelector('.camera-bar-wrapper')).height);
+  getVideoConstraint(menuHeight);
+  initStats();
+
+  // TakePhoto event by clicking takePhotoButton.
+  let takePhotoButton = document.getElementById('takePhotoButton');
+  takePhotoButton.addEventListener('click', function () {
+    // Here we are not using takePhoto() per se.
+    // new ImageCapture(videoTrack) gives image without applied filter.
+    let dstCanvas = document.getElementById('gallery');
+    drawCanvas(dstCanvas, canvasOutput);
+  });
+
+  // TODO(sasha): move to utils.js.
+  let facingModeButton = document.getElementById('facingModeButton');
+  // Switch to face or environment mode by clicking facingModeButton.
+  facingModeButton.addEventListener('click', function () {
+    if (controls.facingMode == 'user') {
+      controls.facingMode = 'environment';
+      videoConstraint.deviceId = { exact: controls.backCamera.deviceId };
+      facingModeButton.innerText = 'camera_front';
+    } else if (controls.facingMode == 'environment') {
+      controls.facingMode = 'user';
+      videoConstraint.deviceId = { exact: controls.frontCamera.deviceId };
+      facingModeButton.innerText = 'camera_rear';
+    }
+    utils.clearError();
+    utils.stopCamera();
+    utils.startCamera(videoConstraint, 'videoInput', startVideoProcessing);
+  });
+
+  if (!isMobileDevice()) {
+    // Init threads number.
+    let threadsControl = document.getElementsByClassName('threads-control')[0];
+    threadsControl.classList.remove('hidden');
+    let threadsNumLabel = document.getElementById('threadsNumLabel');
+    let threadsNum = document.getElementById('threadsNum');
+    threadsNum.max = navigator.hardwareConcurrency;
+    threadsNumLabel.innerHTML = `Number of threads (1 - ${threadsNum.max}):&nbsp;`;
+    if (3 <= threadsNum.max) threadsNum.value = 3;
+    else threadsNum.value = 1;
+    cv.parallel_pthreads_set_threads_num(parseInt(threadsNum.value));
+    threadsNum.addEventListener('change', () => {
+      cv.parallel_pthreads_set_threads_num(parseInt(threadsNum.value));
+    });
+  }
+}
+
+utils.loadOpenCv(() => {
+  utils.createFileFromUrl(faceModelPath, faceModelUrl, () => {
+    utils.createFileFromUrl(emojiModelPath, emojiModelUrl, () => {
+      initUI();
+      initCameraSettingsAndStart();
+    });
+  });
+});
